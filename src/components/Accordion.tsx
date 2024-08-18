afd991ad445c5069431768eca6221550c7079138
GodOfPro <1387ilia@gmail.com>
2024-08-18 23:04:39 +0330

    Scuffed way to do Accordion (WIP)

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,5 +1,32 @@
-const Accordion = () => {
-    return <div>Accordion</div>
+import { ReactNode } from 'react'
+
+interface AccordionProp {
+    title: string
+    isActive: boolean
+    onClick: () => void
+    children: ReactNode
+}
+
+const Accordion = ({ title, children, isActive, onClick }: AccordionProp) => {
+    if (isActive) {
+        return (
+            <button onClick={onClick}>
+                <h2>Accordion - {title}</h2>
+                {children}
+            </button>
+        )
+    } else {
+        return (
+            <div>
+                <button
+                    onClick={onClick}
+                    className="text-2xl text-red-700 underline"
+                >
+                    Accordion - {title} - NOT ACTIVE
+                </button>
+            </div>
+        )
+    }
 }
 
 export default Accordion
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,33 @@
+import { useState } from 'react'
 import Accordion from './Accordion'
-// import EducationForm from './EducationForm'
-// import ExperienceForm from './ExperienceForm'
+import EducationForm from './EducationForm'
+import ExperienceForm from './ExperienceForm'
 import PersonalDetailsForm from './PersonalDetailsForm'
 
 const Sidebar = () => {
+    const [activeIndex, setIndex] = useState(0)
+
+    const handleAccordionClick = (id: number) => {
+        setIndex(id)
+    }
+
     return (
         <div className="w-[450px] bg-red-100">
             <PersonalDetailsForm />
-            {/* props: {title: "Education"} */}
-            <Accordion>{/* <EducationForm /> */}</Accordion>
-            {/* props: {title: "Experience"} */}
-            <Accordion>{/* <ExperienceForm /> */}</Accordion>
+            <Accordion
+                title="Education"
+                isActive={activeIndex === 0}
+                onClick={() => handleAccordionClick(0)}
+            >
+                <EducationForm />
+            </Accordion>
+            <Accordion
+                title="Experience"
+                isActive={activeIndex === 1}
+                onClick={() => handleAccordionClick(1)}
+            >
+                <ExperienceForm />
+            </Accordion>
         </div>
     )
 }
